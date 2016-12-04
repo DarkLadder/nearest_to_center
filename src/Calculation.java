46bb93bc419a0cbd392ffae127bd9f1056c4a552
Dark_Ladder <seriouskeyn@mail.ru>
2016-12-04 21:28:59 +0300

    v1.1
    Код переработан, но не доделан

--- src/Calculation.java
+++ src/Calculation.java
@@ -2,20 +2,16 @@
  * Created by BYT9C0D9 on 22.11.2016.
  */
 public class Calculation {
-    double x, y, l;
+    double x, y;
 
     public Calculation(double x, double y) {
         this.x = x;
         this.y = y;
     }
     public Length Solver() {
+        double l;
         double dis = x*x + y*y;
         l = Math.sqrt(dis);
         return new Length(l);
     }
-    public Length Printer() {
-        System.out.println("Точка имеет кординаты " + "(" + x + "; " + y + ") ее растояние от начала координат = " + l);
-        return new Length(l);
-    }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 /**
  * Created by BYT9C0D9 on 22.11.2016.
  */
@@ -8,14 +7,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Нахождение ближайшего к центру.");
         System.out.println("Сколько всего точек?");
-        int count = sc.nextInt();
-        System.out.println("Введите координаты " + count + " точек");
-        Calculation calc[] = new Calculation[count];
-        for (int i = 0; i < count; i++) {
-            calc[i] = new Calculation(sc.nextDouble(), sc.nextDouble());
-            calc[i].Solver();
-            calc[i].Printer();
-        }
+//        int count = sc.nextInt();
+        System.out.println("Введите координаты точек");
+//        Calculation calc[] = new Calculation[count];
+//        for (int i = 0; i < count; i++) {
+//            calc[i] = new Calculation(sc.nextDouble(), sc.nextDouble());
+//            calc[i].Solver();
+//        }
+        Calculation calc = new Calculation(sc.nextDouble(), sc.nextDouble());
+        System.out.println(calc.Solver());
         sc.close();
     }
 }
