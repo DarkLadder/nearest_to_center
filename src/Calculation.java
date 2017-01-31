8c517f75d87242a69d9304497859f554437c1952
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-01-31 20:27:57 +0300

    v2.0

--- src/Calculation.java
+++ src/Calculation.java
@@ -2,16 +2,10 @@
  * Created by BYT9C0D9 on 22.11.2016.
  */
 public class Calculation {
-    double x, y;
-
-    public Calculation(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-    public Length Solver() {
+    public double solver(double x, double y) {
         double l;
         double dis = x*x + y*y;
         l = Math.sqrt(dis);
-        return new Length(l);
+        return l;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Нахождение ближайшего к центру.");
         System.out.println("Сколько всего точек?");
-//        int count = sc.nextInt();
+        int count = sc.nextInt();
         System.out.println("Введите координаты точек");
-//        Calculation calc[] = new Calculation[count];
-        Calculation calc[] = new Calculation[3];
-        for (int i = 0; i < 3; i++) {
-            calc[i] = new Calculation(sc.nextDouble(), sc.nextDouble());
-            calc[i].Solver();
+        Calculation calc = new Calculation();
+        System.out.print("1. ");
+        double y = calc.solver(sc.nextDouble(), sc.nextDouble());
+        int n = 1;
+        for (int i = 1; i < count; i++) {
+            int n1 = i+1;
+            System.out.print(n1+ ". ");
+            double x = calc.solver(sc.nextDouble(), sc.nextDouble());
+            if (y>x) {y = x; n = n1;}
         }
+        System.out.println("Ближайщая точка имеет номер "+n+ " и имеет длинну " + y);
         sc.close();
     }
 }
