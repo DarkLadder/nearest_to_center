e2f1fe6306d2656411441862c1dec370ab1fd3ac
Dmitrii Dmitriev <seriouskeyn@mail.ru>
2017-02-01 18:42:46 +0300

    v3.0

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,22 @@ public class Main {
         System.out.println("Сколько всего точек?");
         int count = sc.nextInt();
         System.out.println("Введите координаты точек");
-        Calculation calc = new Calculation();
         System.out.print("1. ");
-        double y = calc.solver(sc.nextDouble(), sc.nextDouble());
+        double y = solver(sc.nextDouble(), sc.nextDouble());
         int n = 1;
         for (int i = 1; i < count; i++) {
             int n1 = i+1;
             System.out.print(n1+ ". ");
-            double x = calc.solver(sc.nextDouble(), sc.nextDouble());
+            double x = solver(sc.nextDouble(), sc.nextDouble());
             if (y>x) {y = x; n = n1;}
         }
         System.out.println("Ближайщая точка к началу координат имеет номер "+ n+ " и длинну " + y);
         sc.close();
+    }
+    public static double solver(double x, double y) {
+        double l;
+        double dis = x*x + y*y;
+        l = Math.sqrt(dis);
+        return l;
     }
 }
